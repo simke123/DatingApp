1e007e2bfa31dd8e2275011d414e591b3e6fa2de
Simeon Savic <simeonsimke49@gmail.com>
2024-04-17 13:08:10 +0200

    End of section 4

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
