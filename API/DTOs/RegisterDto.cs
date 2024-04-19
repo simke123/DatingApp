f7cce292a6e3ea26cb6d36da34e7704395102710
Simeon Savic <simeonsimke49@gmail.com>
2024-04-19 11:25:50 +0200

    End of Section 8

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,5 +8,6 @@ public class RegisterDto
 [Required]
 public string Username { get; set; }    
 [Required]
+[StringLength(8,MinimumLength = 4)]
 public string Password { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,8 @@
-using System.Text;
 using API.Data;
 using API.Extensions;
-using API.Interface;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,11 +20,25 @@ var app = builder.Build();
 
 //app.UseHttpsRedirection();
 
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 app.Run();
